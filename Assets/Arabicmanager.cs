dbb0b1d6d2fc671b1d22ec1b2aa6a5b9eca03d09
semihcagatay <semihcagatay1@gmail.com>
2023-09-09 17:00:10 +0300

    cari v1.2

--- Assets/Arabicmanager.cs
+++ Assets/Arabicmanager.cs
@@ -6,20 +6,46 @@ public class Arabicmanager : MonoBehaviour
 {
     Animator animator;
     // Start is called before the first frame update
-    bool canAttack = true;
+    bool canAttack = false;
+    bool isAlive = true;
+    int attackTime=3;
+    float randomX;
+    float randomY;
+    
     void Start()
     {
         animator = GetComponent<Animator>();
+        StartCoroutine(setAttack());
+        randomX = Random.Range(-10.0f,6.0f);
+        randomY = Random.Range(0, 2.5f);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (canAttack)
-        {
-            animator.SetBool("CanAttack", true);
+        animator.SetBool("CanAttack", canAttack);
+    }
 
+    IEnumerator setAttack()
+    {
+        while (isAlive)
+        {
+            yield return new WaitForSeconds(attackTime);
+            canAttack = !canAttack;
         }
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        animator.SetBool("Teleport",true);
+    }
+    public void teleport()
+    {
+        transform.position=new Vector2(randomX, randomY);
+        //animator.SetBool("Reback",true);
+        animator.SetBool("Teleport", false); 
+        randomX = Random.Range(-10.0f, 6.0f);
+        randomY = Random.Range(0, 2.5f);
+    }
+
 }
