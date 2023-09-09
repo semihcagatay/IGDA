7e5ccb13b056825a28ea728af2b947f6c5d8e616
semihcagatay <semihcagatay1@gmail.com>
2023-09-09 14:15:49 +0300

    cariv1.2

---
+++ Assets/Arabicmanager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arabicmanager : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    bool canAttack = true;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (canAttack)
+        {
+            animator.SetBool("CanAttack", true);
+
+        }
+    }
+    IEnumerator 
+}
